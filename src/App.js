f6f4b0e626995a28256fee138f889c51cf218506
tanner <tannersimpkins@gmail.com>
2022-08-27 00:10:29 -0500

    Centered package names in card

--- src/App.js
+++ src/App.js
@@ -313,7 +313,7 @@ const App = () => {
 
             <div className="flex flex-wrap items-center mt-32">
               <div class="p-4 m-auto my-10 w-full md:w-3/12 bg-white rounded-lg border shadow-md sm:p-8 bg-gray-600 dark:border-gray-700">
-                <h5 class="mb-4 text-xl font-medium text-white">Bronze Package</h5>
+                <h5 class="mb-4 text-xl font-medium text-white text-center">Bronze Package</h5>
                   <div class="flex items-baseline text-gray-900 dark:text-white">
                     <span class="text-3xl font-semibold">$</span>
                     <span class="text-5xl font-extrabold tracking-tight">350</span>
@@ -333,19 +333,19 @@ const App = () => {
                         <span class="text-base font-normal leading-tight text-white">Sample</span>
                     </li>
                     <li class="flex space-x-3 line-through decoration-gray-500">
-                        <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                        <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
                         <span class="text-base font-normal leading-tight text-white">Sample</span>
                     </li>
                     <li class="flex space-x-3 line-through decoration-gray-500">
-                        <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                        <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
                         <span class="text-base font-normal leading-tight text-white">Sample</span>
                     </li>
                     <li class="flex space-x-3 line-through decoration-gray-500">
-                        <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                        <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
                         <span class="text-base font-normal leading-tight text-white">Sample</span>
                     </li>
                     <li class="flex space-x-3 line-through decoration-gray-500">
-                        <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-gray-400 dark:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+                        <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
                         <span class="text-base font-normal leading-tight text-white">Sample</span>
                     </li>
                 </ul>
@@ -354,7 +354,7 @@ const App = () => {
 
 
               <div class="p-4 m-auto my-10 w-full md:w-3/12 bg-white rounded-lg border shadow-md sm:p-8 bg-gray-600 dark:border-gray-700">
-                <h5 class="mb-4 text-xl font-medium text-white">Silver Package</h5>
+                <h5 class="mb-4 text-xl font-medium text-white text-center">Silver Package</h5>
                   <div class="flex items-baseline text-gray-900 dark:text-white">
                     <span class="text-3xl font-semibold">$</span>
                     <span class="text-5xl font-extrabold tracking-tight">650</span>
@@ -395,7 +395,7 @@ const App = () => {
 
               
               <div class="p-4 m-auto my-10 w-full md:w-3/12 bg-white rounded-lg border shadow-md sm:p-8 bg-gray-600 dark:border-gray-700">
-                <h5 class="mb-4 text-xl font-medium text-white">Gold Package</h5>
+                <h5 class="mb-4 text-xl font-medium text-white text-center">Gold Package</h5>
                   <div class="flex items-baseline text-gray-900 dark:text-white">
                     <span class="text-3xl font-semibold">$</span>
                     <span class="text-5xl font-extrabold tracking-tight">1000</span>
