dabdf7dea5712c6a8d1cd356616eb0be7d03269c
tanner <tannersimpkins@gmail.com>
2022-08-27 00:01:58 -0500

    Fixed linter warning

--- src/App.js
+++ src/App.js
@@ -319,7 +319,7 @@ const App = () => {
                     <span class="text-5xl font-extrabold tracking-tight">350</span>
                     <span class="ml-1 text-xl font-normal text-white">/month</span>
                   </div>
-                  <ul role="list" class="my-7 space-y-5">
+                  <ul class="my-7 space-y-5">
                     <li class="flex space-x-3">
                       <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
                       <span class="text-base font-normal leading-tight text-white">2 team members</span>
@@ -360,7 +360,7 @@ const App = () => {
                     <span class="text-5xl font-extrabold tracking-tight">650</span>
                     <span class="ml-1 text-xl font-normal text-white">/month</span>
                   </div>
-                  <ul role="list" class="my-7 space-y-5">
+                  <ul class="my-7 space-y-5">
                     <li class="flex space-x-3">
                       <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
                       <span class="text-base font-normal leading-tight text-white">2 team members</span>
@@ -401,7 +401,7 @@ const App = () => {
                     <span class="text-5xl font-extrabold tracking-tight">1000</span>
                     <span class="ml-1 text-xl font-normal text-white">/month</span>
                   </div>
-                  <ul role="list" class="my-7 space-y-5">
+                  <ul class="my-7 space-y-5">
                     <li class="flex space-x-3">
                       <svg aria-hidden="true" class="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
                       <span class="text-base font-normal leading-tight text-white">2 team members</span>
